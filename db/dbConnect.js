db92eca1b660d0edd0ea2c0fb68d20b8ae12269b
Malyaj <malyajsingh09@gmail.com>
2023-02-02 18:11:52 +0530

    update

--- db/dbConnect.js
+++ db/dbConnect.js
@@ -1,13 +1,12 @@
 // external imports
 const mongoose = require("mongoose");
+require('dotenv').config();
 
-require('dotenv').config()
-const DB_URL="mongodb+srv://cody:CmdCody2224@project0.zuduho3.mongodb.net/?retryWrites=true&w=majority";
 async function dbConnect() {
   // use mongoose to connect this app to our database on mongoDB using the DB_URL (connection string)
   mongoose
     .connect(
-      DB_URL,
+      process.env.DB_URL,
       {
         //   these are options to ensure that the connection is done properly
         useNewUrlParser: true,
